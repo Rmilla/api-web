173f5c8bc25efb2095f456f5ffe308a522402219
Robin MILLA <robin.milla.pro@gmail.com>
2023-11-23 13:42:22 +0100

    23/11/2023 13:42

--- src/models/ouvrage.py
+++ src/models/ouvrage.py
@@ -9,11 +9,11 @@ class Ouvrage:
     titre_ouvrage: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
     auteur_ouvrage : Mapped[str] = mapped_column(String(30))
     prix: Mapped[int] = mapped_column(int)
-    isbn_ouvrage: Mapped[str] = mapped_column() #VARCHAR(20),
-    langue_ouvrage: Mapped[str] = mapped_column() #VARCHAR(20),
-    prix_ouvrage: Mapped[float] = mapped_column() #DECIMAL(10, 2),
-    date_parution_ouvrage: Mapped[datetime] = mapped_column() #DATE,
-    categorie_ouvrage: Mapped[str] = mapped_column() #VARCHAR(255),
+    isbn_ouvrage: Mapped[str] = mapped_column(str(20)) #VARCHAR(20),
+    langue_ouvrage: Mapped[str] = mapped_column(str(20)) #VARCHAR(20),
+    prix_ouvrage: Mapped[float] = mapped_column(float(10,2)) #DECIMAL(10, 2),
+    date_parution_ouvrage: Mapped[datetime] = mapped_column(datetime) #DATE,
+    categorie_ouvrage: Mapped[str] = mapped_column(str(255)) #VARCHAR(255),
     date_disponibilite_libraire_ouvrage: Mapped[datetime] = mapped_column(datetime) #DATE,
     date_disponibilite_particulier_ouvrage: Mapped[datetime] = mapped_column(datetime) #DATE,
     image_ouvrage: Mapped[str] = mapped_column(String(255)) #VARCHAR(255),
